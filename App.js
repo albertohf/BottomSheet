a6687cf07ad798c2b543ff3ffbf229cca11eb411
Albertohf <albertohf91@gmail.com>
2024-01-15 19:06:32 -0300

    first commit

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet from '@gorhom/bottom-sheet'
+import {useRef, useMemo } from 'react'
+
 
 export default function App() {
+  const bottomSheetRef = useRef(null);
+  const snapPoints = useMemo(() => ['35%', '60%'], []);
+
+  const handleSheetClose = () => {
+    bottomSheetRef.current?.close()
+  }
+  const handleSheetOpen = () => {
+    bottomSheetRef.current?.expand()
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Button title="Open" onPress={() => {handleSheetOpen}} />
+      
+
+      <View style={styles.container}>
+        <BottomSheet 
+          ref={bottomSheetRef}
+          index={1}
+          snapPoints={snapPoints}
+          backgroundStyle={{backgroundColor: "#FFF"}}
+          enablePanDownToClose={true}
+        >
+          <View style={styles.contentContainer}>
+            <Text>Hello World</Text>
+            <Button title="Close" onPress={() => {handleSheetClose}} />
+          </View>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
